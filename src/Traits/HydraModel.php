3cf643ffa4d7f7484c5af450fe3144e21ee838db
Exidus <admin@exidus.sk>
2019-11-20 16:59:56 +0100

    Initial commit #2

---
+++ src/Traits/HydraModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Exidus\Hydra\Traits;
+
+use Illuminate\Support\Facades\Auth;
+
+trait HydraModel
+{
+
+    protected static $validationFields;
+
+    public static function getTableName()
+    {
+        return ((new self)->getTable());
+    }
+
+    public static function rules(array $data = null)
+    {
+        return [];
+    }
+
+    public static function rulesOnly(array $fiter, array $data = null)
+    {
+        return array_intersect_key(self::rules($data), array_flip($fiter));
+    }
+
+    public static function rulesData(array $data = null, string $field = null)
+    {
+        if (is_array($data) && isset($data[$field])) {
+            return $data[$field];
+        }
+        return null;
+    }
+
+    public function simpleAuth($field = 'user_id', $value = null)
+    {
+        if (!$value) {
+            $value = Auth::id();
+        }
+        if ($this->{$field} != $value) {
+            abort(403, 'Access denied.');
+        }
+        return $this;
+    }
+
+}
